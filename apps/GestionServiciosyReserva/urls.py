5abecbe01343799a294d4136e983ffeeffd5bffa
yohancv <yohanquenta0@gmail.com>
2026-04-07 18:08:19 -0400

    Arreglando las urls

--- apps/GestionServiciosyReserva/urls.py
+++ apps/GestionServiciosyReserva/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-   CategoriaServicioListCreateView,
+    CategoriaServicioListCreateView,
     CategoriaServicioDetailView,
     ServicioListCreateView,
     ServicioDetailView,
@@ -9,12 +9,15 @@ from .views import (
 )
 
 urlpatterns = [
+    # Categorías
     path('categorias-servicio/', CategoriaServicioListCreateView.as_view(), name='categoria-list-create'),
-    path('categorias-servicio/<int:pk>/', CategoriaServicioDetailView.as_view(), name='categoria-detail'),
+    path('categorias/<int:pk>/', CategoriaServicioDetailView.as_view(), name='categoria-detail'),
 
-    path('servicios/', ServicioListCreateView.as_view(), name='servicio-list-create'),
-    path('servicios/<int:pk>/', ServicioDetailView.as_view(), name='servicio-detail'),
+    # Servicios
+    path('', ServicioListCreateView.as_view(), name='servicio-list-create'),
+    path('<int:pk>/', ServicioDetailView.as_view(), name='servicio-detail'),
 
+    # Precios
     path('precios-servicio/', PrecioServicioListCreateView.as_view(), name='precio-list-create'),
-    path('precios-servicio/<int:pk>/', PrecioServicioDetailView.as_view(), name='precio-detail'),
+    path('precios/<int:pk>/', PrecioServicioDetailView.as_view(), name='precio-detail'),
 ]
