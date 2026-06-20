c5baedaa09243ef4d54a2c04350e80445b141539
Stefy127 <noeste1234@gmail.com>
2026-06-20 01:29:22 -0400

    Implementar KPIs del dashboard

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     KPIsView,
+    DashboardKPIsView,
     ListStaticReportsView,
     GenerarEstaticoView,
     GenerarDinamicoView,
@@ -11,6 +12,7 @@ from .views import (
 
 urlpatterns = [
     path("kpis/", KPIsView.as_view(), name="reportes-kpis"),
+    path("dashboard-kpis/", DashboardKPIsView.as_view(), name="reportes-dashboard-kpis"),
     path("estaticos/", ListStaticReportsView.as_view(), name="reportes-estaticos-list"),
     path("estaticos/generar/", GenerarEstaticoView.as_view(), name="reportes-estaticos-generar"),
     path("dinamicos/generar/", GenerarDinamicoView.as_view(), name="reportes-dinamicos-generar"),
